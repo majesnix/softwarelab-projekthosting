d460e0dcc32b7964c50c551d0e4b5504cfa3c194
majesnix <dominic@dominic-classen.de>
2017-11-28 00:08:05 +0100

    sql fieldname correction, fix usersettings, avatar now changeable

--- app/controller/projectController.js
+++ app/controller/projectController.js
@@ -4,6 +4,7 @@ module.exports.createProject = async (req, res) => {
   //create DB entry in projectdatabase (FK -> Matrikelnummer)
   //create folders for project
 };
+
 module.exports.deleteProject = async (req, res) => {
   //delete folders of the project
   //delete DB entry
@@ -13,14 +14,16 @@ module.exports.createApplication = async (req, res) => {
   //create new entry in application db (FK -> Projectid)
   //create folder for application
 };
+
 module.exports.deleteApplication = async (req, res) => {
   //delete folder of application
   //delete DB entry
 };
 
 module.exports.createDatabase = async (req, res) => {
-  //create new database (FK -> projectid)
+  //create new database (FK -> Projectid)
 };
+
 module.exports.deleteDatabase = async (req, res) => {
   //delete database
 };
--- routes/adminsettings.js
+++ routes/adminsettings.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const userController = require('../app/controller/userController');
 
 router.get('/', require('connect-ensure-login').ensureLoggedIn('/'), (req, res, next) => {
-  res.locals = req.flash();
+  res.locals.message = req.flash();
   res.render('adminsettings', {
     user: require('../user.json'),
     user2: req.session.passport.user,
-    message: res.locals.error || res.locals.info
+    message: res.locals.message.error || res.locals.message.info
   });
 });
 
